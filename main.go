eef370f4a86aa612c4d61ccaa710b77b2f8c77c3
SKKUGoon <baesangil0906@gmail.com>
2022-03-23 15:22:09 +0900

    Separate broadcasting module to goServer repository v1.0.0

--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	//"goBinance/broadcast"
 	"goBinance/crawler"
-	"log"
-	//"net/http"
 )
 
 func main() {
-	log.Println("Starting Trading Session:")
-	//broadcast.SetupRoutes()
 	a, err := crawler.CrawlUpbit()
 	if err == nil {
 		fmt.Println(a)
 	}
-	//log.Fatal(http.ListenAndServe(":7890", nil))
 }
