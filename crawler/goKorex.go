5993b52be826e2f799b018b70a31e16b6eb12278
SKKUGoon <baesangil0906@gmail.com>
2022-04-29 01:38:34 +0900

    parse time only in bithumb parse parse asset uplist in bithumb with time

--- crawler/goKorex.go
+++ crawler/goKorex.go
@@ -2,6 +2,7 @@ package crawler
 
 import (
 	"errors"
+	"fmt"
 	"golang.org/x/net/html"
 	"log"
 	"strings"
@@ -29,6 +30,7 @@ func clean(s []byte) (string, error) {
 
 func ifAssetKor(text string) ([]string, error) {
 	var titleAsset []string
+
 	for _, c := range strings.Fields(text) {
 		a, err := clean([]byte(c))
 		if err == nil {
@@ -37,6 +39,7 @@ func ifAssetKor(text string) ([]string, error) {
 			}
 		}
 	}
+
 	if len(titleAsset) <= 0 {
 		return titleAsset, errors.New("no asset found")
 	} else {
@@ -50,7 +53,6 @@ func AssetUpbit(title UpbitTitle) ([]string, error) {
 	if strings.Contains(t, MARKETADDUPBIT) {
 		data, err := ifAssetKor(t)
 		if err != nil {
-			log.Println(data)
 			return nil, errors.New("no asset found")
 		} else {
 			return data, nil
@@ -60,45 +62,88 @@ func AssetUpbit(title UpbitTitle) ([]string, error) {
 	}
 }
 
-func extractDate(tp html.TokenType, t html.Token) (interface{}, error) {
+func extractDate(tp html.TokenType, t html.Token) (time.Time, error) {
+	/*
+		/ Bithumb gives out only time information
+		/ prstLayout -> iff the information is uploaded that day
+		/ histLayout -> historical posts.
+		/ location -> always kst base
+	*/
+	tNow := time.Now()
+	prstLayout := " 15:04"     // HH:MM
+	histLayout := "2006.01.02" // YYYY.MM.DD
+	tloc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		log.Panic("incorrect timezone location")
+	}
+
+	// parse dates
 	if tp == html.TextToken {
-		layout := "2006.01.02"
-		td, err := time.Parse(layout, t.Data)
-		if err != nil {
-			return nil, errors.New("not a date")
-		} else {
-			return td, nil
+		switch {
+		case strings.Contains(t.Data, ":"):
+			td, err := time.ParseInLocation(prstLayout, t.Data, tloc)
+			// Add dates. -1 since it the default dates starts at 0000-01-01
+			td = td.AddDate(tNow.Year(), int(tNow.Month())-1, tNow.Day()-1)
+
+			if err != nil {
+				return time.Time{}, errors.New("cannot parse today's date")
+			} else {
+				return td, nil
+			}
+
+		default:
+			td, err := time.ParseInLocation(histLayout, t.Data, tloc)
+
+			if err != nil {
+				return time.Time{}, errors.New("cannot parse historical date")
+			} else {
+				return td, nil
+			}
 		}
 	} else {
-		return nil, errors.New("not html text token")
+		return time.Time{}, errors.New("not html text token")
 	}
 }
 
-func extractAsset(tp html.TokenType, t html.Token) {
+func extractAsset(tp html.TokenType, t html.Token) ([]string, error) {
 	if tp == html.TextToken {
-		asset, err := ifAssetKor(t.Data)
-		if err != nil {
-			log.Println("err", err)
+		// If MARKET ADD event
+		contain1 := strings.Contains(t.Data, MARKETADDBITHUMB2)
+		contain2 := strings.Contains(t.Data, MARKETADDBITHUMB3)
+
+		if contain1 || contain2 {
+			asset, err := ifAssetKor(t.Data)
+			if err != nil {
+				return nil, errors.New("market add but parsing error")
+			} else {
+				return asset, nil
+			}
 		} else {
-			log.Println("asset", asset)
+			return nil, errors.New("not asset statement")
 		}
+	} else {
+		return nil, errors.New("not text token")
 	}
 }
 
-func AssetBithumb(text string, wanted map[string]bool) ([]string, error) {
+func AssetBithumb(text string, wanted map[string]bool) ([]time.Time, error) {
 	/*
 		/ bithumb does not provide news api
 		/ parse the raw html files.
 		/ crawl the TextToken that comes after <a> StartToken: asset name
 		/ crawl the TextToken that comes after <td> StartToken: date
 	*/
+	var timeContainer []time.Time
+	var assetContainer [][]string
+
 	tkn := html.NewTokenizer(strings.NewReader(text))
 	for {
 		tt := tkn.Next()
+
 		switch {
 		// End of Parsing
 		case tt == html.ErrorToken:
-			return nil, errors.New("end of parse")
+			return timeContainer, nil
 
 		// ex) <a> </a> <= <a> is a StartTagToken
 		case tt == html.StartTagToken:
@@ -107,20 +152,24 @@ func AssetBithumb(text string, wanted map[string]bool) ([]string, error) {
 			switch {
 			// process datetime
 			case wanted[tn.Data] == true && tn.Data == "td":
-				tt = tkn.Next()
-				tn = tkn.Token()
+				tt, tn = tkn.Next(), tkn.Token()
+
 				td, err := extractDate(tt, tn)
-				if err != nil {
-					continue
-				} else {
-					log.Println(td)
+				if err == nil {
+					fmt.Println("timeContainer", td)
+					timeContainer = append(timeContainer, td)
 				}
 
 			// process asset content
 			case wanted[tn.Data] == true && tn.Data == "a":
-				tt = tkn.Next()
-				tn = tkn.Token()
-				extractAsset(tt, tn)
+				tt, tn = tkn.Next(), tkn.Token()
+
+				asset, err := extractAsset(tt, tn)
+				if err == nil {
+					fmt.Println("assetContainer", asset)
+					assetContainer = append(assetContainer, asset)
+				}
+
 			default:
 				continue
 			}
