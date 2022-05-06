572d08984645eccb10effebf5fdd2c4820dd8348
SKKUGoon <baesangil0906@gmail.com>
2022-05-06 19:35:33 +0900

    TODO: create strategies ordersheet

--- orderbook/orderCreate.go
+++ orderbook/orderCreate.go
@@ -1,1 +1,16 @@
 package orderbook
+
+//func Strategy1(asset string) OrderContent {
+//	s1 := OrderContent{
+//		A: asset,
+//		N: domestic.UpbitOrder
+//	}
+//}
+//
+//func Strategy2() {
+//
+//}
+//
+//func Strategy3() {
+//
+//}
